83539664262f390702b356cb15f9667e113d9094
ahmed-aa2309476 <ahmedatiah96@gmail.com>
2026-06-22 14:54:00 +0300

    transfer files into src folder

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI # type: ignore
 from dotenv import load_dotenv # type: ignore
 load_dotenv(".env")
 
-from routes import base
+from src.routes import base
 
 app = FastAPI()
 
